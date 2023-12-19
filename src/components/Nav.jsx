b50263212cc424f81d79e5a2b72d17bfa9b90486
ramanboff <bayramofframan@gmail.com>
2023-12-19 17:21:18 +0400

    used  cardContext  for  items length in Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { FaShoppingBag } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import CardContext from "../CardContext";
 
 const Nav = () => {
+ const {items} = useContext(CardContext)
+
   return (
     <nav className="w-full bg-orange-200 px-6 py-4 text-gray-600 font-bold">
       <div className="flex justify-between items-center bg-orange-200">
@@ -11,7 +15,7 @@ const Nav = () => {
        <Link to={"/checkout"}>
         <div className="flex items-center gap-2">
           <FaShoppingBag />
-          <span>0</span>
+          <span>{items.length}</span>
         </div>
         </Link>
       </div>
