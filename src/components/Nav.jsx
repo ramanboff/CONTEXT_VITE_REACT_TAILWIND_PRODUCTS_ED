e62e68372c2a8152bfa92f4c66e717b1fc41f30d
ramanboff <bayramofframan@gmail.com>
2023-12-19 15:45:24 +0400

    Logo and  shoppingCardIcon  wrapped by Link from router-dom

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,19 @@
 import { FaShoppingBag } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className="w-full bg-orange-200 px-6 py-4 text-gray-600 font-bold">
       <div className="flex justify-between items-center bg-orange-200">
-        <div>Wear</div>
+       <Link to={"/"}>
+       <h1>Wear</h1>
+       </Link>
+       <Link to={"/checkout"}>
         <div className="flex items-center gap-2">
           <FaShoppingBag />
           <span>0</span>
         </div>
+        </Link>
       </div>
     </nav>
   );
