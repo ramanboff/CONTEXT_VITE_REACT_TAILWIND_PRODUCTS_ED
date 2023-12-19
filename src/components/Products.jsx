e6c0bc7f88ef30f8261bf5f85255e1f66ba8b144
ramanboff <bayramofframan@gmail.com>
2023-12-19 15:35:53 +0400

    added products  state to Products component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,21 @@
+import Card from "./Card";
+
 const Products = () => {
-  return ( 
-    <div>
-      Products
+  const products = [
+    { name: "vest", price: 50 },
+    { name: "jacket", price: 70 },
+    { name: "sweater", price: 20 },
+    { name: "tshirt", price: 40 },
+    { name: "necklace", price: 70 },
+    { name: "jumper", price: 60 },
+  ];
+  return (
+    <div className="grid grid-cols-1 py-10 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {products.map((product) => (
+        <Card name={product.name} price={product.price}/>
+      ))}
     </div>
-   );
-}
- 
+  );
+};
+
 export default Products;
