ee095488af7750c42cbf61e0a67e09bea8a0f759
ramanboff <bayramofframan@gmail.com>
2023-12-19 17:19:26 +0400

    added items state  and  addToCard function  to CardContext component

--- src/CardContext.jsx
+++ src/CardContext.jsx
@@ -1,10 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const CardContext = createContext();
 
 export function CardProvider({ children }) {
+  //add to card function
+  const [items, setItems] = useState([]);
+  const addToCard = (name, price) => {
+    setItems((prevState) => [...prevState, { name, price }]);
+  };
+  //add to card function
+
   return (
-    <CardContext.Provider value={{ item: 1 }}>{children}</CardContext.Provider>
+    <CardContext.Provider value={{ items, addToCard }}>
+      {children}
+    </CardContext.Provider>
   );
 }
 
