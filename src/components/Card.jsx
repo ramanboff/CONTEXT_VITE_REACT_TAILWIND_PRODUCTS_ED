4e2092ece88d920602dd9f26e27686545f1307f5
ramanboff <bayramofframan@gmail.com>
2023-12-19 15:52:31 +0400

    cards bg changed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,15 @@ import tshirt from "../../public/tshirt.png";
 import { FaShoppingBag } from "react-icons/fa";
 const Card = ({ name, price }) => {
   return (
-    <div className="bg-gray-300 rounded text-gray-600 font-bold py-6">
-      <div className="px-2">
+    <div className="bg-orange-100 rounded text-gray-600 font-bold py-6">
+      <div className="px-6 flex justify-center items-center">
         <img src={tshirt} alt="product" />
       </div>
-      <div className="flex justify-between items-center px-6 ">
+      <div className="flex justify-between items-center px-12 mt-3 ">
         <h3>{name}</h3>
         <FaShoppingBag />
       </div>
-      <h4 className="px-6">${price}</h4>
+      <h4 className="px-12">${price}</h4>
     </div>
   );
 };
