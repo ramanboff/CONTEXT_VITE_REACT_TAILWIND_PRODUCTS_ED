3e368ac60ad0c80272633f71cf13c1b20e2072da
ramanboff <bayramofframan@gmail.com>
2023-12-19 21:51:40 +0400

    card style updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import { useContext } from "react";
-import tshirt from "../../public/black-tshirt.png";
+import tshirt from "../../public/w.png";
 import { FaShoppingBag } from "react-icons/fa";
 import CardContext from "../CardContext";
 const Card = ({ name, price }) => {
   const { addToCard } = useContext(CardContext);
   return (
     <div className="bg-orange-100 rounded text-gray-600 font-bold cursor-pointer">
-      <div className="px-10 pt-2 flex justify-center items-center">
-        <img src={tshirt} alt="product" />
+      <div className="pt-2 flex justify-center items-center p-3">
+        <img src={tshirt} alt="product" className="object-cover" />
       </div>
       <div
         className="flex justify-between items-center
@@ -16,7 +16,7 @@ const Card = ({ name, price }) => {
         <h3>{name}</h3>
         <button
           onClick={() => addToCard(name, price)}
-          className="flex gap-1  bg-orange-400 rounded-full px-3 py-1 hover:bg-black hover:text-white duration-300"
+          className="flex items-end gap-1  bg-orange-300 rounded-full px-3 py-1 hover:bg-black hover:text-white duration-300"
         >
           <p className="text-[14px] ">add</p>
           <FaShoppingBag className="text-xl" />
